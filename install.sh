d66582701808de37f2497403cfcb12d0e2f05996
Daniel Mulvey <danmulvey@gmail.com>
2022-11-10 13:51:36 -0800

    creating install script skeleton

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# this is a simple script to link our dotfiles
+
+# if the file already exists, move it to a backup folder
+echo "backing up existing dotfiles"
+
+# link the dotfiles
+echo "linking dotfiles in ~/.dotfiles"
